73a3529cd30a1f91a6415718f9581694879a454d
ValeriaKul <lera.dribnyuk@mail.ru>
2023-03-03 11:53:14 +0200

    Task 27 Jan 2023

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0)
-  const [allClicks, setAllClicks] = useState([])
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: "React Router DOM",
+    },
+    {
+      id: 2,
+      text: "Axios vs Fetch",
+    },
+  ]);
 
-  const handleLeftClick = () => {
-    setLeft(left + 1)
-    setAllClicks([...allClicks, 'L'])
-  }
-
-  const handleRightClick = () => {
-    setRight(right + 1)
-    setAllClicks([...allClicks, 'R'])
-  }
   return (
     <div className="App">
-      {left}
-      <button onClick={handleLeftClick}>
-        left
-      </button>
-
-      <button onClick={handleRightClick}>
-        right
-      </button>
-      {right}
-      <p>History of cliks: {allClicks.join(' ')}</p>
+      <h1>Temes: </h1>
+      <ul>
+        <li>{notes[0].text}</li>
+        <li>{notes[1].text}</li>
+      </ul>
     </div>
   );
 }
 
 export default App;
 // Задача. При нажатии на кнопку left, менять свойство left на 1
-// При нажатии на right менять свойства right на 1 
+// При нажатии на right менять свойства right на 1
 
 // Задача. При нажатии на кнопку left, увеличивать состояние left на 1
 // При нажатии на кнопку right, увелиичивать состояние right на 1
@@ -41,12 +34,12 @@ export default App;
 // left = 0, left = 0 + 1, left = 1
 // left = 1, left = 1 + 1, left = 2
 // let left = 0
-  // left = left + 1
-  // setLeft(left + 1) - говорит реакту перерисоваться
-  // left++
-  // left = left + 1
+// left = left + 1
+// setLeft(left + 1) - говорит реакту перерисоваться
+// left++
+// left = left + 1
 
-  /*  После первого клика по left
+/*  После первого клика по left
     const newClicks = {
       left: 1 (clicks.left = 0, clicks.left + 1 = 1),
       right: clicks.right
